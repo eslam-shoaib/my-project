7b1eb7af0801f34451ccc3f34c678ef6c1c700f6
Eslam Shoaib Abd-el gaber <59358260+eslam-shoaib@users.noreply.github.com>
2020-04-07 14:33:46 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+﻿function time() {
+    var now = new Date(),
+    hours = now.getHours(),
+    minutes = now.getMinutes(),
+    seconds = now.getSeconds();
+
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    } 
+
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    } ;
+    document.getElementById("clock").textContent = (hours + ":" + minutes + ":" + seconds);
+
+    };
+
+
+    
+   window.onload = function repeat() {
+        setInterval(time , 500);
+        };
